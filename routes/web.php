4b9c578430bb97d0f0818ee5cb7a69dda1484509
jimbv <joseignaciomartin@gmail.com>
2022-11-25 16:18:14 -0300

    test commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/invitacion/{codigo}', function ($codigo) {
+    
+    return view('invitacion', ['codigo' => $code]); 
+});
